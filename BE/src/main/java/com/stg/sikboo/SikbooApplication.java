b48ad228cfa91dbde3e2ecd0f2705d47856bcd9d
ncb6206 <ncb6206@gmail.com>
2025-11-03 17:58:44 +0900

    init: spring boot 환경설정

---
+++ BE/src/main/java/com/stg/sikboo/SikbooApplication.java
@@ -0,0 +1,13 @@
+package com.stg.sikboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikbooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikbooApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/stg/sikboo/SikbooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stg.sikboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikbooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
