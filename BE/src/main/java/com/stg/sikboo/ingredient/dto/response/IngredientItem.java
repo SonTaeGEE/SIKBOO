0ffceb0652be806f98afce50e3f876c74fcab035
jiwon <wldnjs9397@hanmail.net>
2025-11-13 14:43:03 +0900

    BE: 페이징 오류 수정, 소비기한 계산 로직 수정

--- BE/src/main/java/com/stg/sikboo/ingredient/dto/response/IngredientItem.java
+++ BE/src/main/java/com/stg/sikboo/ingredient/dto/response/IngredientItem.java
@@ -6,7 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 @Setter
 @NoArgsConstructor
@@ -29,6 +31,12 @@ public class IngredientItem {
     
     public String getStorage() {
         return cleanString(storage);
+    }
+    
+ // ✅ expiryDays는 기본 Getter 사용 (int는 정제 불필요)
+    public int getExpiryDays() {
+        log.debug("IngredientItem.getExpiryDays() 호출: {}", expiryDays);
+        return expiryDays;
     }
     
     private String cleanString(String str) {
